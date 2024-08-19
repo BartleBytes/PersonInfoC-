6bd4888185bd72745f5b21e059b321d5312644f6
dylanbartle <dylanbartle@hotmail.com>
2024-08-18 20:14:00 -0600

    files

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    // Declare variables to hold the personal information
+    string firstName = "Basil";
+    string lastName = "Bartle";
+    string streetAddress = "1234 Elm Street";
+    string city = "Fictionville";
+    string zipCode = "12345";
+
+    // Print out the fictional person's information
+    cout << "Fictional Person's Information: " << endl;
+    cout << "First Name: " << firstName << endl;
+    cout << "Last Name: " << lastName << endl;
+    cout << "Street Address: " << streetAddress << endl;
+    cout << "City: " << city << endl;
+    cout << "Zip Code: " << zipCode << endl;
+
+    return 0;
+}
